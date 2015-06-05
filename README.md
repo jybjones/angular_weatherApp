918a2c4e967ab41f5c49df64ad348cf0d0717236
Jennifer Jones <jybjones@gmail.com>
2015-06-05 15:57:57 -0500

    geolocation and temperature working

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # angular_weatherApp
+API key: 7b7b50d6c7c89ebd
---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+angular
+  .module('angular_weatherApp', [])
+
+  .controller('GeolocationController', function ($http){
+    var vm = this;
+
+    navigator.geolocation.getCurrentPosition(function (geoposition) {
+      var lat = geoposition.coords.latitude;
+      var long = geoposition.coords.longitude;
+
+      $http
+      .get(`http://api.wunderground.com/api/7b7b50d6c7c89ebd/conditions/q/${lat},${long}.json`)
+      .success(function (data) {
+        vm.temp_f = data.current_observation.temp_f;
+      });
+    });
+
+  });
+
+
+
+
+
