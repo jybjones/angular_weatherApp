0dcf0b281eff91842ce1d938267f67d6437c382c
Jennifer Jones <jybjones@gmail.com>
2015-06-07 16:15:51 -0500

    weather lcoation working, created ngRoute and views file

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,52 @@
 angular
-  .module('angular_weatherApp', [])
+  // .module('angular_weatherApp', [])
+  // .controller('GeolocationController', function ($http){
+    .module('angular_weatherApp', ['ngRoute'])
+    .config(function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          controller: 'GeolocationController',
+          controllerAs: 'weather',
+          templateUrl: 'views/current.html'
+        })
+        .when('/:zip', {
+          controller: 'GeolocationController',
+          controllerAs: 'weather',
+          templateUrl: 'views/current.html'
+        });
+    })
 
-  .controller('GeolocationController', function ($http){
-    var vm = this;
+    .controller('GeolocationController', function ($http, $routeParams) {
+      var vm = this;
 
-    navigator.geolocation.getCurrentPosition(function (geoposition) {
-      var lat = geoposition.coords.latitude;
-      var long = geoposition.coords.longitude;
+      navigator.geolocation.getCurrentPosition(function (geoposition) {
+        var lat = geoposition.coords.latitude;
+        var long = geoposition.coords.longitude;
+      vm.zip = +$routeParams.zip;
 
-      $http
-      .get(`http://api.wunderground.com/api/7b7b50d6c7c89ebd/conditions/q/${lat},${long}.json`)
-      .success(function (data) {
-        vm.temp_f = data.current_observation.temp_f;
+      if (vm.zip) {
+        $http
+          .get(`http://api.wunderground.com/api/7b7b50d6c7c89ebd/conditions/q/${lat},${long}.json`)
+          .get(`http://api.wunderground.com/api/7b7b50d6c7c89ebd/conditions/q/${vm.zip}.json`)
+          .success(function (data) {
+            vm.temp_f = data.current_observation.temp_f;
+          });
+      } else {
+        navigator.geolocation.getCurrentPosition(function (geoposition) {
+        var lat = geoposition.coords.latitude;
+        var long = geoposition.coords.longitude;
+
+         $http
+          .get(`http://api.wunderground.com/api/7b7b50d6c7c89ebd/conditions/q/${lat},${long}.json`)
+          .success(function (data) {
+            vm.temp_f = data.current_observation.temp_f;
+          });
+
       });
-    });
-      $http
-      .get(`http://api.wunderground.com/api/7b7b50d6c7c89ebd/conditions/q/ + zip + , function (data) {
-
-};
-
+    }
+      vm.zipLookup = function (){
+        window.location = '/#/' + vm.zip;
+      };
   });
-
-
-
-
+});
 
